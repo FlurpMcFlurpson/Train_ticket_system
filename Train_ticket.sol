2f51b6aa833cc61b71cce69b377d0dbf62436de8
Matthew Eash <75139104+FlurpMcFlurpson@users.noreply.github.com>
2023-11-14 16:20:53 -0500

    Update

--- Train_ticket.sol
+++ Train_ticket.sol
@@ -10,15 +10,16 @@ contract Train_tickets {
         address buyer;
     }
 
-    mapping(uint256 => Ticket) public tickets;
+    mapping(address => uint256) public wallets;
+    mapping(address => Ticket) public tickets;
     mapping(address => bool) public members;
 
     event Ticket_Purchased(uint256 Ticket_ID, address buyer);
     event Member_Registerd(address member);
 
-    constructor(uint256 ticket_Price) {
+    constructor(uint256 set_Price) {
         owner = msg.sender;
-        ticketPrice = ticket_Price;
+        ticketPrice = set_Price;
     }
 
     function register() external {
@@ -28,10 +29,46 @@ contract Train_tickets {
         emit Member_Registerd(msg.sender);
     }
 
-    function buy_ticket(uint256 Ticket_ID) public payable {
-        require(msg.value >= ticketPrice, "Tansaction Failed");
+    function buy_ticket() public payable {
+        require(wallets[msg.sender] > 0, "Tansaction Failed");
 
-        tickets[Ticket_ID].buyer = msg.sender;
-        emit Ticket_Purchased(Ticket_ID, msg.sender);
+        if (members[msg.sender] == true) {
+            require(
+                wallets[msg.sender] >= ticketPrice / 2,
+                "Tansaction Failed"
+            );
+            wallets[msg.sender] -= ticketPrice / 2;
+            tickets[msg.sender].buyer = msg.sender;
+            tickets[msg.sender].ticketId = 1;
+            emit Ticket_Purchased(tickets[msg.sender].ticketId, msg.sender);
+        } else {
+            require(wallets[msg.sender] >= ticketPrice, "Tansaction Failed");
+            wallets[msg.sender] -= ticketPrice / 2;
+            tickets[msg.sender].buyer = msg.sender;
+            tickets[msg.sender].ticketId = 1;
+            emit Ticket_Purchased(tickets[msg.sender].ticketId, msg.sender);
+        }
+    }
+
+    function set_price(uint new_Price) public {
+        require(owner == msg.sender, "Access Denied");
+        ticketPrice = new_Price;
+    }
+
+    function set_balance() public payable {
+        require(msg.value > 0, "Must deposit amount more than zero!");
+        wallets[msg.sender] += msg.value;
+    }
+
+    function get_balnace() public view returns (uint256) {
+        return wallets[msg.sender];
+    }
+
+    function withdraw(uint256 amount) public {
+        require(amount > 0, "Can not withdraw 0");
+        require(wallets[msg.sender] >= amount, "Trasaction Failed");
+
+        wallets[msg.sender] -= amount;
+        payable(msg.sender).transfer(amount);
     }
 }
