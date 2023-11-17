c86a0a112e04a721017d979c00c412a7a1b8670c
Matthew Eash <75139104+FlurpMcFlurpson@users.noreply.github.com>
2023-11-17 16:08:40 -0500

    all features loosely added

--- Train_ticket-app/app.js
+++ Train_ticket-app/app.js
@@ -1,9 +1,13 @@
-const incrementButton = document.getElementById('increment-button');
-const decrementButton = document.getElementById('decrement-button');
+const set_balance_Button = document.getElementById('setbalance-button');
+const Withdraw_button = document.getElementById('Withdraw-button');
+const CheakButton = document.getElementById('Cheak-button');
+const Balance = document.getElementById('Balance');
 const counterValue = document.getElementById('Ticket_Price');
-const lastUpdated = document.getElementById('last_updated');
-const counterAddress = '0x57F1B19198aC15858b1162546C5DfB042Cbd355a';
-const counterABI = [
+const register = document.getElementById('Member-Reg');
+const new_Price_button = document.getElementById('new_price_button')
+const buy_ticket_button = document.getElementById('buy_ticket_button')
+const Address = '0xD45Eb5B7D5A794a5E48Ccc9962BD061DCC0Bc4Cf';
+const ABI = [
     {
         "inputs": [
             {
@@ -205,16 +209,57 @@ const counterABI = [
     }
 ]
 const web3 = new Web3('http://localhost:7545');
-const Trian = new web3.eth.Contract(counterABI, counterAddress);
+const Trian = new web3.eth.Contract(ABI, Address);
 
 window.addEventListener('load', async (event) => {
     const value = await get_price();
-    counterValue.innerHTML = value;
+    const etherValue = Web3.utils.fromWei(value, 'ether');
+    counterValue.innerHTML = etherValue;
     console.log("updated");
 })
 
+set_balance_Button.onclick = async () => {
+    const accounts = await web3.eth.getAccounts();
+    const value = document.getElementById('setbalance').value;
+    const WeiValue = Web3.utils.toWei(value, 'ether');
+    await Trian.methods.set_balance().send({ from: accounts[1], value: WeiValue });
+}
+
+CheakButton.onclick = async () => {
+    const accounts = await web3.eth.getAccounts();
+    const value = await Trian.methods.get_balnace().call({ from: accounts[1] });
+    const etherValue = Web3.utils.fromWei(value, 'ether');
+    Balance.innerHTML = etherValue;
+
+}
+
+Withdraw_button.onclick = async () => {
+    const accounts = await web3.eth.getAccounts();
+    const value = document.getElementById('Withdraw').value;
+    const WeiValue = Web3.utils.toWei(value, 'ether');
+    await Trian.methods.withdraw(WeiValue).send({ from: accounts[1] });
+
+}
 
 async function get_price() {
     const value = await Trian.methods.get_price().call();
     return value;
+}
+
+register.onclick = async () => {
+    const accounts = await web3.eth.getAccounts();
+    await Trian.methods.register().send({ from: accounts[1] })
+}
+
+new_Price_button.onclick = async () => {
+    const accounts = await web3.eth.getAccounts();
+    const value = document.getElementById('new_price').value
+    const etherValue = web3.utils.toWei(value, 'ether');
+    await Trian.methods.set_price(etherValue).send({ from: accounts[0] })
+}
+
+
+buy_ticket_button.onclick = async () => {
+    const accounts = await web3.eth.getAccounts();
+    await Trian.methods.buy_ticket().send({ from: accounts[1] })
 }
